e3ec69a2ab680f6ec919c9e063677e51da2e6228
nzabalqis <karenza.balqis@gmail.com>
2022-01-11 18:23:14 +0700

    list

---
+++ pertemuan 2/list.py
@@ -0,0 +1,13 @@
+mylist = []
+mylist.append(1) #tambah 1 anggota bernilai 1
+mylist.append(2) #tambah 1 anggota bernilai 2
+mylist.append(3) #tambah 1 anggota bernilai 3
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+print(mylist)
+
+for x in mylist:
+    print(x)
+    
