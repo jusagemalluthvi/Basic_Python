3b2d298790fdfe975706b02620bdb6b8aa32ac0a
nzabalqis <karenza.balqis@gmail.com>
2022-01-11 18:41:47 +0700

    2

---
+++ materi/Basic Python #1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Hello world!
+
+# In[ ]:
+
+
+# Ini untuk menampilkan ke layar
+# print("Hello, Farhan!")
+print("Hello World!")
+
+
+# # Variable
+
+# In[ ]:
+
+
+angka = 10
+y = "Hello, Farhan!"
+
+print(angka)
+
